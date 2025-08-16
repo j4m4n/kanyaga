50e02f32d7db5430687b6a0be1202b908e700136
j4m4n <jaman@brukaisoft.com>
2025-08-16 18:43:29 -0400

    Update IntroScene.js

--- 64/js/classes/IntroScene.js
+++ 64/js/classes/IntroScene.js
@@ -27,8 +27,40 @@ class IntroScene {
     const bsStandCanvasArr = sliceCanvas(bsStandImg, 64, 64);
     const childEncounterCanvasArr = sliceCanvas(childEncounterImg, 64, 64);
 
+    const chargedBotCanvasArr = sliceCanvas(chargedBotImg, 64, 64);
+
+    const kidAweCanvasArr = sliceCanvas(kidAweImg, 64, 64);
+    const kidHappyCanvasArr = sliceCanvas(kidHappyImg, 64, 64);
+
+    const engiHappyCanvasArr = sliceCanvas(engiHappyImg, 64, 64);
+    const engiShockCanvasArr = sliceCanvas(engiShockImg, 64, 64);
+    const engiSmirkCanvasArr = sliceCanvas(engiSmirkImg, 64, 64);
+
+    const workshopChargingCanvasArr = sliceCanvas(workshopChargingImg, 64, 64);
+    const kidRunCanvasArr = sliceCanvas(kidRunImg, 64, 64);
+    const beepBoopCanvasArr = sliceCanvas(beepBoopImg, 64, 64);
+
+    const workshopChargingAltCanvasArr = sliceCanvas(workshopChargingAltImg, 64, 64);
+    const workshopChargingStableCanvasArr = sliceCanvas(workshopChargingStableImg, 64, 64);
+
+    const ACTORS = {
+      dad: {
+        happy: engiHappyCanvasArr[0],
+        shock: engiShockCanvasArr[0],
+        smirk: engiSmirkCanvasArr[0],
+      },
+      kid: {
+        happy: kidHappyCanvasArr[0],
+        awe: kidAweCanvasArr[0],
+      },
+      bot: {
+        charged: chargedBotCanvasArr[0],
+      }
+    };
+    // document.body.appendChild(kidAweCanvasArr[0]);
+
     solarFlareCanvasArr = solarFlareCanvasArr.slice(0, -1);
-    splashDownCanvasArr = splashDownCanvasArr.slice(0, -2);
+    // splashDownCanvasArr = splashDownCanvasArr.slice(0, -2);
 
     // console.log("splashDownCanvasArr.length:", splashDownCanvasArr.length);
 
@@ -42,11 +74,12 @@ class IntroScene {
     frames.push({ canvas: ftlCanvasArr[2], duration: 150, text: INTRO_TEXT, sfx: 'btn_c' });
     frames.push({ canvas: ftlCanvasArr[3], duration: 150, text: INTRO_TEXT, sfx: 'btn_e' });
     frames.push({ canvas: ftlCanvasArr[4], duration: 150, text: INTRO_TEXT });
-    frames.push({ canvas: ftlCanvasArr[5], duration: 150, text: INTRO_TEXT, sfx: 'btn_e' });
+    frames.push({ canvas: ftlCanvasArr[5], duration: 150, text: INTRO_TEXT, sfx: 'btn_g' });
     frames.push({ canvas: ftlCanvasArr[6], duration: 150, text: INTRO_TEXT });
-    frames.push({ canvas: ftlCanvasArr[7], duration: 150, text: INTRO_TEXT, sfx: 'btn_g' });
-
-
+    frames.push({ canvas: ftlCanvasArr[7], duration: 150, text: INTRO_TEXT, sfx: 'btn_r' });
+    
+    
+    frames.push({ canvas: solarFlareCanvasArr[0], duration: 150, sfx: 'ftl_arrive' });
     frames.push(
       ...solarFlareCanvasArr.map((n,i) => {
         return { canvas: n, duration: 250, sfx: i===2 ? 'solar_flare' : undefined };
@@ -57,21 +90,22 @@ class IntroScene {
         return {
           canvas: n,
           duration: 250,
-          text: 'Tap to stabilize!',
+          text: 'Tap to STABILIZE!',
           sfx: i === 0 ? 'out_of_control' : undefined,
         };
       })
     );
-    frames.push({ canvas: aimForPlanetCanvasArr[0], duration: 150, sfx: 'ftl' });
-    frames.push({ canvas: aimForPlanetCanvasArr[0], duration: 150, text: 'Systems critical!' });
-    frames.push(
-      ...aimForPlanetCanvasArr.map((n,i) => {
-        return {
-          canvas: n,
-          duration: 250
-        };
-      })
-    );
+    frames.push({ canvas: aimForPlanetCanvasArr[0], duration: 150, sfx: 'stabilized' });
+
+    frames.push({ canvas: aimForPlanetCanvasArr[0], duration: 150, sfx: 'stabilized' });
+    frames.push({ canvas: aimForPlanetCanvasArr[1], duration: 150});
+    frames.push({ canvas: aimForPlanetCanvasArr[2], duration: 150, sfx: 'btn_c' });
+    frames.push({ canvas: aimForPlanetCanvasArr[3], duration: 150, sfx: 'btn_e' });
+    frames.push({ canvas: aimForPlanetCanvasArr[4], duration: 150 });
+    frames.push({ canvas: aimForPlanetCanvasArr[5], duration: 150, sfx: 'btn_g' });
+    frames.push({ canvas: aimForPlanetCanvasArr[6], duration: 150 });
+    frames.push({ canvas: aimForPlanetCanvasArr[7], duration: 150, sfx: 'btn_r' });
+
     frames.push({ canvas: aimForPlanetPauseCanvasArr[0], duration: 150, text: 'Attempt to land!', sfx: 'ftl' });
     frames.push({ canvas: aimForPlanetPauseCanvasArr[1], duration: 150, text: 'Attempt to land!' });
     frames.push({ canvas: aimForPlanetPauseCanvasArr[2], duration: 150, text: 'Attempt to land!', sfx: 'btn_c' });
@@ -79,7 +113,7 @@ class IntroScene {
     frames.push({ canvas: aimForPlanetPauseCanvasArr[4], duration: 150, text: 'Attempt to land!' });
     frames.push({ canvas: aimForPlanetPauseCanvasArr[5], duration: 150, text: 'Attempt to land!', sfx: 'btn_fs' });
     frames.push({ canvas: aimForPlanetPauseCanvasArr[6], duration: 150, text: 'Attempt to land!' });
-    frames.push({ canvas: aimForPlanetPauseCanvasArr[7], duration: 150, text: 'Attempt to land!', sfx: 'btn_cs' });
+    frames.push({ canvas: aimForPlanetPauseCanvasArr[7], duration: 150, text: 'Attempt to land!', sfx: 'btn_r' });
     
     frames.push(
       ...flyToPlanetCanvasArr.map((n,i) => {
@@ -91,6 +125,7 @@ class IntroScene {
       })
     );
     // frames.push({ canvas: flyToPlanetCanvasArr[flyToPlanetCanvasArr.length - 1], duration: 500, text: 'Crash imminent!', sfx: 'alarm_cs' });
+    frames.push({ canvas: flyToPlanetCanvasArr[flyToPlanetCanvasArr.length - 1], duration: 500, text: 'Crash imminent!', sfx: 'alarm_r' });
     frames.push({ canvas: flyToPlanetCanvasArr[flyToPlanetCanvasArr.length - 1], duration: 500, text: 'Crash imminent!', sfx: 'alarm_c' });
     frames.push({ canvas: flyToPlanetCanvasArr[flyToPlanetCanvasArr.length - 1], duration: 500, text: 'Crash imminent!', sfx: 'alarm_fs' });
     
@@ -127,18 +162,14 @@ class IntroScene {
     frames.push({canvas: bsLayCanvasArr[4], duration: 350, text: "..."});
     frames.push({canvas: bsLayCanvasArr[5], duration: 350, text: "..."});
     frames.push(
-      ...bsLayCanvasArr.map((n,i) => {
-        return {canvas: n,duration: 350};
-      })
-    );
-    frames.push(
       ...bsShineCanvasArr.map((n,i) => {
         return {canvas: n,duration: 350};
       })
     );
+    frames.push({canvas: bsLaySunCanvasArr[0], duration: 350, text: "Solar charging...", sfx: "solar_charging"});
     frames.push(
       ...bsLaySunCanvasArr.map((n,i) => {
-        return {canvas: n,duration: 350, text: "Charging..."};
+        return {canvas: n,duration: 350, text: "Solar charging..."};
       })
     );
     frames.push(
@@ -146,6 +177,7 @@ class IntroScene {
         return {canvas: n,duration: 350};
       })
     );
+    frames.push({canvas: bsSitCanvasArr[0], duration: 350, text: "Calibrating...", sfx: "calibrating"});
     frames.push(
       ...bsSitCanvasArr.map((n,i) => {
         return {canvas: n,duration: 350, text: "Calibrating..."};
@@ -153,7 +185,11 @@ class IntroScene {
     );
     frames.push(
       ...bsGetUpCanvasArr.map((n,i) => {
-        return {canvas: n,duration: 350};
+        return {
+          canvas: n,
+          duration: 350,
+          sfx: i === 3 ? 'footstep_sandy' : undefined
+        };
       })
     );
     frames.push({canvas: bsStandCanvasArr[0], duration: 350, text: "Scanning...", sfx: 'scanning'});
@@ -162,11 +198,162 @@ class IntroScene {
     frames.push({canvas: bsStandCanvasArr[3], duration: 350, text: "Scanning..."});
     frames.push({canvas: bsStandCanvasArr[4], duration: 350, text: "Scanning..."});
     frames.push({canvas: bsStandCanvasArr[5], duration: 350, text: "Scanning..."});
+
+    frames.push({canvas: childEncounterCanvasArr[0], duration: 350, text: "Fauna detected.", sfx: 'fauna_detected'});
     frames.push(
       ...childEncounterCanvasArr.map((n,i) => {
         return {canvas: n,duration: 350, text: "Fauna detected."};
       })
     );
+
+    frames.push({canvas: kidAweCanvasArr[0], duration: 350, text: "Wow who are you?!", sfx: 'talk3'});
+    frames.push({canvas: kidAweCanvasArr[0], duration: 350, text: "Wow who are you?!"});
+
+    frames.push({canvas: beepBoopCanvasArr[0], duration: 350, text: "*beep boop*", sfx: 'beep'});
+    frames.push({canvas: beepBoopCanvasArr[1], duration: 350, text: "*beep boop*"});
+    frames.push({canvas: beepBoopCanvasArr[2], duration: 350, text: "*beep boop*", sfx: 'boop'});
+    frames.push({canvas: beepBoopCanvasArr[3], duration: 350, text: "*beep boop*"});
+
+    frames.push({canvas: kidHappyCanvasArr[0], duration: 350, text: "You're a robot!", sfx: 'talk3'});
+    frames.push({canvas: kidHappyCanvasArr[0], duration: 350, text: "You're a robot!"});
+
+    frames.push({canvas: bsLayCanvasArr[0], duration: 350, text: "Energy low...", sfx: 'power_down'});
+    frames.push(
+      ...bsLayCanvasArr.map((n,i) => {
+        return {canvas: n,duration: 350, text: "Energy low..."};
+      })
+    );
+
+    frames.push({canvas: kidAweCanvasArr[0], duration: 455, text: "Are you okay?", sfx: 'talk3'});
+    frames.push({canvas: kidAweCanvasArr[0], duration: 455, text: "I'll get dad!", sfx: 'talk3'});
+
+    frames.push({canvas: kidRunCanvasArr[0], duration: 150, sfx: 'footstep_sandy'});
+    frames.push({canvas: kidRunCanvasArr[1], duration: 150});
+    frames.push({canvas: kidRunCanvasArr[2], duration: 150, sfx: 'footstep_sandy'});
+    frames.push({canvas: kidRunCanvasArr[3], duration: 150});
+    frames.push({canvas: kidRunCanvasArr[4], duration: 150, sfx: 'footstep_sandy'});
+    frames.push({canvas: kidRunCanvasArr[5], duration: 150});
+    frames.push({canvas: kidRunCanvasArr[6], duration: 150, sfx: 'footstep_sandy'});
+    frames.push({canvas: kidRunCanvasArr[7], duration: 150});
+    frames.push({canvas: kidRunCanvasArr[8], duration: 150, sfx: 'footstep_sandy'});
+    
+    frames.push({canvas: bsLayCanvasArr[0], duration: 350, text: "5 minutes later...", sfx: 'beach_wave'});
+    frames.push({canvas: bsLayCanvasArr[1], duration: 350, text: "5 minutes later..."});
+    frames.push({canvas: bsLayCanvasArr[2], duration: 350, text: "5 minutes later..."});
+    frames.push({canvas: bsLayCanvasArr[3], duration: 350, text: "5 minutes later..."});
+    frames.push({canvas: bsLayCanvasArr[4], duration: 350, text: "5 minutes later..."});
+    frames.push({canvas: bsLayCanvasArr[5], duration: 350, text: "5 minutes later..."});
+
+    frames.push({canvas: engiShockCanvasArr[0], duration: 650, text: "It's a BEAT BOT!", sfx: 'talk4'});
+
+    frames.push({canvas: kidHappyCanvasArr[0], duration: 455, text: "Can you fix it?", sfx: 'talk3'});
+    frames.push({canvas: engiSmirkCanvasArr[0], duration: 650, text: "Yes, but don't tell your mother.", sfx: 'talk4'});
+    frames.push({canvas: kidHappyCanvasArr[0], duration: 455, text: "I won't, I swear!", sfx: 'talk3'});
+    frames.push({canvas: engiHappyCanvasArr[0], duration: 650, text: "Okay... to the workshop!", sfx: 'talk4'});
+
+    frames.push(
+      ...workshopChargingCanvasArr.map((n,i) => {
+        return {
+          canvas: n,
+          duration: i < 16 ? 150 : 350,
+          text: "Almost got it...",
+          sfx: i < 16 ? 'keyboard_clack' : 'workshop_charging'
+        };
+      })
+    );
+    frames.push(
+      ...workshopChargingAltCanvasArr.map((n,i) => {
+        let sfx = i === 17 || i === 19 || i === 21 ? 'scratch' : undefined;
+        if(i < 16) sfx = 'keyboard_clack';
+        return {
+          canvas: n,
+          duration: 150,
+          text: "Hmm...",
+          sfx
+        };
+      })
+    );
+    frames.push(
+      ...workshopChargingCanvasArr.map((n,i) => {
+        return {
+          canvas: n,
+          duration: i < 16 ? 150 : 350,
+          text: "Ah okay...",
+          sfx: i < 16 ? 'keyboard_clack' : 'workshop_charging'
+        };
+      })
+    );
+    frames.push(
+      ...workshopChargingStableCanvasArr.map((n,i) => {
+
+        return {
+          canvas: n,
+          duration: 150,
+          text: "I GOT IT!",
+          sfx: i === 3 ? 'belly_jiggle' : undefined
+        };
+      })
+    );
+    frames.push({
+      canvas: ACTORS.kid.happy,
+      duration: 455,
+      text: "His eyes glow!",
+      sfx: 'talk3'
+    });
+    frames.push({
+      canvas: ACTORS.dad.shock,
+      duration: 455,
+      text: "Go get Vera!",
+      sfx: 'talk4'
+    });
+    frames.push({
+      canvas: ACTORS.dad.shock,
+      duration: 455,
+      text: "Tell her to bring her DJ equipment!",
+      sfx: 'talk4'
+    });
+    frames.push({
+      canvas: ACTORS.kid.awe,
+      duration: 455,
+      text: "...what?!",
+      sfx: 'talk3'
+    });
+    frames.push({
+      canvas: ACTORS.dad.shock,
+      duration: 455,
+      text: "Just go! Hurry!",
+      sfx: 'talk4'
+    });
+    frames.push({
+      canvas: ACTORS.bot.charged,
+      duration: 455,
+      text: "Take me to your DJ",
+      sfx: 'bot_talk'
+    });
+    frames.push({
+      canvas: ACTORS.dad.shock,
+      duration: 455,
+      text: "She's on her way",
+      sfx: 'talk3'
+    });
+    frames.push({
+      canvas: ACTORS.bot.charged,
+      duration: 455,
+      text: "No... NOW!",
+      sfx: 'bot_talk'
+    });
+    frames.push({
+      canvas: ACTORS.dad.shock,
+      duration: 455,
+      text: "Then I will DJ!",
+      sfx: 'talk3'
+    });
+    frames.push({
+      canvas: ACTORS.bot.charged,
+      duration: 455,
+      text: "Very well...",
+      sfx: 'bot_talk'
+    });
 
     this.cfp = new CanvasFramePlayer(target, frames, 12, () => this.onQuit());
     this.cfp.play();
